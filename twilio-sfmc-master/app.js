3ba659d32d667b85cd8fe86c0e2134ba7a2a7341
wjchoi <wonjeung.choi@amorepacific.com>
2021-09-23 21:00:58 +0900

    3

--- twilio-sfmc-master/app.js
+++ twilio-sfmc-master/app.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
   console.log(req.body, req);
 
   console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
-  //console.log('PORT: '+req.PORT);
+  console.log('PORT: '+req.PORT);
   next();
 });
 
