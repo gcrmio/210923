856b41a941dbddd0a209f95faff2eefe1c25dfe6
wjchoi <wonjeung.choi@amorepacific.com>
2021-09-23 20:18:53 +0900

    5

--- twilio-sfmc-master/app.js
+++ twilio-sfmc-master/app.js
@@ -38,10 +38,7 @@ app.post('/journeybuilder/validate/', activity.validate );
 app.post('/journeybuilder/publish/', activity.publish );
 app.post('/journeybuilder/execute/', activity.execute );
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-
-// check
+//check
 app.use((req, res, next) => {
   console.log('*****************************************');
   console.log('%s', req);
@@ -50,4 +47,6 @@ app.use((req, res, next) => {
   next();
 });
 
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
