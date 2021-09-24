13c20f6eb55b3ca8310b56e6b0fc71c02fcc0c7c
wjchoi <wonjeung.choi@amorepacific.com>
2021-09-24 09:55:48 +0900

    333

--- twilio-sfmc-master/app.js
+++ twilio-sfmc-master/app.js
@@ -19,7 +19,7 @@ app.set('port', process.env.PORT || 3000);
 app.use((req, res, next) => {
   console.log('*****************************************');
   //console.log('%s', req);
-  console.log(JSON.strinfigy(req.body, req));
+  console.log(JSON.stringify(req.body, req));
   //console.log(req.body, req);  
   console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
   //console.log('PORT: '+req.PORT);
